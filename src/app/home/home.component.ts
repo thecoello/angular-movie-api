2b0eb7718b2b64a8abc3eb650e1ee8b5864fab8e
Manuel Alejandro Guerra Coello <thecoello@gmail.com>
2023-05-09 17:42:10 +0200

    Test de Bootstrap en componente Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiMovieService } from '../services/api-movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  movies: Array<any> = []
+  constructor(private apiMovieService: ApiMovieService){}
+
+  ngOnInit():void{
+    this.apiMovieService.getMovies('the+avengers').subscribe((data) =>{
+      console.log(data.results)
+      this.movies = data.results
+    },
+    (error)=>{
+      console.log(error)
+    }
+    )
+  }
 
 }
