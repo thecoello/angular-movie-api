06241d2065f050bd2e351c919a643941af51bb25
Manuel Alejandro Guerra Coello <thecoello@gmail.com>
2023-05-16 17:52:46 +0200

    Obtener datos de API para mostrar las películas usando el modelo movies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiMovieService } from '../services/api-movie.service';
+import { Movies } from '../models/movies-response.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-  movies: Array<any> = []
+  movies:Movies[]=[]
   constructor(private apiMovieService: ApiMovieService){}
 
   ngOnInit():void{
     this.apiMovieService.getMovies('the+avengers').subscribe((data) =>{
-      console.log(data.results)
-      this.movies = data.results
+     this.movies = data.results;
+     console.log(this.movies)
     },
     (error)=>{
       console.log(error)
