29b9d8612cfcdde5a0876518888b0d181ad7420a
Manuel Guerra Coello <thecoello@gmail.com>
2023-05-24 20:09:59 +0200

    Implementación de componente home en componente app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TitlesComponent } from './Movies/titles/titles.component';
+import { SliderComponent } from './Movies/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TitlesComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
