c250728c243863919867946dfcfbafab6b3069d6
Manuel Guerra Coello <thecoello@gmail.com>
2023-05-24 20:10:40 +0200

    Modificación de componente home para utilizarlo como componente "principal" en dónde se mostrarán todos los componentes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiMovieService } from '../services/api-movie.service';
-import { Movies } from '../models/movies-response.model';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  movies:Movies[]=[]
-  constructor(private apiMovieService: ApiMovieService){}
-
-  ngOnInit():void{
-    this.apiMovieService.getMovies('the+avengers').subscribe((data) =>{
-     this.movies = data.results;
-     console.log(this.movies)
-    },
-    (error)=>{
-      console.log(error)
-    }
-    )
-  }
 
 }
