6d879fcc684288568baa5b9cf78345d7f5805a5a
Manuel Guerra Coello <thecoello@gmail.com>
2023-06-05 23:44:04 +0200

    Eliminación de menú e implementación de buscador con botón y filtro de Movie o TV Show

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
 
+
+export class HomeComponent   {
+  titleMovie: string = ""
+  titleType: string = "Movie"
+  findtype: string = ""
+  findMovie: string = ""
+
+
+  findTitle(): void {
+    this.findMovie = this.titleMovie
+    this.findtype = this.titleType
+
+  }
 
 }
