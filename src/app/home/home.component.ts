572c234fe243028faa38a5642d2571bde39b6562
Manuel Guerra Coello <thecoello@gmail.com>
2023-06-11 18:50:34 +0200

    -Implementación de métodos para obtener Id getIdTVShow y getIdTVMovie permitiendo establecer así una comunicación con otros componentes. -Implementación de método GoBack para la comunicación desde componente title-info y limpiar variables en cuanto se de click al botón GoBak para volver a la pantalla principal. -Implementación de método reload para refrescar la página en cuanto se de click a la palabra ScREEN en la página principal.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core'
 
 @Component({
   selector: 'app-home',
@@ -6,18 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 
-
-export class HomeComponent   {
+export class HomeComponent {
   titleMovie: string = ""
   titleType: string = "Movie"
   findtype: string = ""
   findMovie: string = ""
-
+  setIdMovie: any = ''
+  setIdShow: any = ''
+  currentYear: string = new Date().getFullYear().toString()
 
   findTitle(): void {
     this.findMovie = this.titleMovie
     this.findtype = this.titleType
+  }
 
+  getIdTVShow(id: any):void{
+    this.setIdShow = id
+    this.setIdMovie = ''
+  }
+
+  getIdMovie(id: any):void{
+    this.setIdMovie = id
+    this.setIdShow = ''
+  }
+
+  goBack(empty: any){
+    this.titleMovie = ''
+    this.findtype = ''
+    this.findMovie = ''
+    this.setIdMovie = ''
+    this.setIdShow = ''
+  }
+
+  reload(){
+    window.location.reload();
   }
 
 }
