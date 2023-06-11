91ef943b799c5c3a275c089063818c5da9d6e6a3
Manuel Guerra Coello <thecoello@gmail.com>
2023-06-11 18:45:22 +0200

    Modificación de texto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { TitlesComponent } from './Movies/titles/titles.component';
-import { SliderComponent } from './Movies/slider/slider.component';
+import { TitlesComponent } from './Titles/titles/titles.component';
+import { SliderComponent } from './Titles/slider/slider.component';
 import { FormsModule } from '@angular/forms';
+import { TitleInfoComponent } from './Titles/title-info/title-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TitlesComponent,
-    SliderComponent
+    SliderComponent,
+    TitleInfoComponent
   ],
   imports: [
     BrowserModule,
